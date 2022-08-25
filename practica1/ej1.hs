8e7ac74ab049b95d59041955a7f0d8298efb2420
LeandroGomez1 <leandrodanielgomez@gmail.com>
2022-08-24 23:45:57 -0300

    Ejercicio 1 terminado

--- practica1/ej1.hs
+++ practica1/ej1.hs
@@ -1,1 +1,40 @@
+five :: Num p1 => p2 -> p1
 five x = 5
+
+apply :: p -> p
+apply f = f
+
+id :: p -> p
+id x = x
+
+first :: p1 -> p2 -> p1
+first x y = x
+
+sign :: (Ord a, Num a) => a -> Char
+sign x = if x > 0
+        then '+'
+        else '-'
+
+absSign :: (Ord a, Num a) => a -> a
+absSign x = if sign x == '+'
+            then x
+            else negate x
+
+absNoSign :: (Ord a, Num a) => a -> a
+absNoSign x = if x > 0
+        then x
+        else negate x
+
+pot :: (Integral b, Num a) => b -> a -> a
+pot exp x = x ^ exp
+
+max3 :: Ord a => a -> a -> a -> a
+max3 x1 x2 x3
+  | x1 >= x2 && x1 >= x3 = x1
+  | x2 >= x1 && x2 >= x3 = x2
+  | otherwise = x3
+
+--max3 x1 x2 x3 = maximum [x1, x2, x3]
+
+swap :: t1 -> (t1 -> t2) -> t2
+swap x y = y x
