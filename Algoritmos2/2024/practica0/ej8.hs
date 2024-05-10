723d9bf3c961bbc21681917c37b2d47b7c226923
Leandro Gómez <leandrodanielgomez@gmail.com>
2024-05-10 12:05:15 -0300

    Practica 0 completada

--- Algoritmos2/2024/practica0/ej8.hs
+++ Algoritmos2/2024/practica0/ej8.hs
@@ -2,6 +2,9 @@
 {-# HLINT ignore "Use or" #-}
 {-# HLINT ignore "Eta reduce" #-}
 {-# HLINT ignore "Use sum" #-}
+{-# HLINT ignore "Use and" #-}
+{-# HLINT ignore "Use isAsciiLower" #-}
+{-# HLINT ignore "Use isAsciiUpper" #-}
 module Practica0 where
 
 import Data.List
@@ -45,23 +48,51 @@ buscar c ((x,i):xs)
 --e) 'restos', que calcula la lista de los restos de la
 --división de los elementos de una lista de números dada por otro
 --número dado
+restos :: (Num a, Ord a) => a -> [a] -> [a]
+restos n xs = map (resto n) xs
+
+resto :: (Num a, Ord a) => a -> a -> a
+resto n x = x - divisionEntera x n * n
+
+divisionEntera :: (Num a, Ord a) => a -> a -> a
+divisionEntera x n = divisionEntera' x n 1
+
+divisionEntera' :: (Num a, Ord a) => a -> a -> a -> a
+divisionEntera' _ 0 _ = error "No es posible dividir por cero."
+divisionEntera' x n d
+    | n * d < x = divisionEntera' x n (d + 1)
+    | n * d == x = d
+    | otherwise = d-1
 
 --f) 'cuadrados', que dada una lista de números, devuelva la
 --lista de sus cuadrados
 
+cuadrados :: (Num a) => [a] -> [a]
+cuadrados xs = map (^2) xs
 --g) 'longitudes', que dada una lista de listas, devuelve la
 --lista de sus longitudes
+
+longitudes :: [[a]] -> [Int]
+longitudes xss = map length xss
 
 --h) 'orden', que dada una lista de pares de números, devuelve
 --la lista de aquellos pares en los que la primera componente es
 --menor que el triple de la segunda
+orden :: (Num a, Ord a) => [(a,a)] -> [(a,a)]
+orden ((x,y):xs) = filter (\(x,y) -> x < (y*3)) xs
 
 --i) 'pares', que dada una lista de enteros, devuelve la lista
 --de los elementos pares
+pares :: [Int] -> [Int]
+pares xs = filter even xs
 
 --j) 'letras', que dada una lista de caracteres, devuelve la
 --lista de aquellos que son letras (minúsculas o mayúsculas)
+letras :: [Char] -> [Char]
+letras xs = filter (\x -> x >= 'a' && x <= 'z' || x >= 'A' && x <= 'Z') xs
 
 --k) 'masDe', que dada una lista de listas 'xss' y un
 --número 'n', devuelve la lista de aquellas listas de 'xss'
 --con longitud mayor que 'n'
+masDe :: [[a]] -> Int -> [[a]]
+masDe xss n = filter (\x -> length x > n) xss
